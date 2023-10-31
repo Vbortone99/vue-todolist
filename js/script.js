46f9b54638897e28102d746c7fd097f669322b8f
Vbortone99 <bortonevincenzo99@gmail.com>
2023-10-31 17:43:42 +0100

    Vue Instance structure

--- js/script.js
+++ js/script.js
@@ -1,10 +1,30 @@
 const createApp = Vue;
 createApp({
     data(){
-
+        return{
+            tasks:[
+                {
+                    id:1,
+                    text:'Sveglia alle 8:00',
+                    done:true,
+                },
+                {
+                    id:2,
+                    text:'Collegamento Zoom in classe',
+                    done:false,
+                },
+                {
+                    id:3,
+                    text:'Finisci la lezione',
+                    done:false,
+                },
+            ]
+        }
     },
+    lastId:3,
+    todoText:'',
     methods(){
 
     }
-})
+}).mount('#app')
 
